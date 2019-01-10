e964a541a903a5bd0270aab1e7ae72308278bdda
Dylan Barva <d.barva5402@gmail.com>
2019-01-10 11:54:06 -0800

    only has drivetrain stuff

---
+++ src/main/java/com/nerdherd/robot/Robot.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+  /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.nerdherd.robot;
+
+// import com.nerdherd.robot.subsystems.Drive;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+/**
+ * 
+ */
+public class Robot extends TimedRobot {
+	
+	public static final String kDate = "2018_09_29_";
+
+	// public static Drive drive;
+
+	@Override
+	public void robotInit() {
+		// drive = new Drive();
+	}
+
+	@Override
+	public void disabledInit() {
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void autonomousInit() {
+		
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+    public void testPeriodic() {
+    }
+}
